4367c14959454d75197bf1c3ab9386c501b84747
Berikbol Manaikhan <nn.z.icon@gmail.com>
2024-05-15 03:36:55 +0500

    app

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ const QRCode = require("./model/qrCode");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
 app.use(express.json());
 
 app.post("/user", async (req, res) => {
